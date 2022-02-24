1b35cffd483d9cee8907aa165a0e13fa429030d0
jmeno1011 <jwhltn8282@gmail.com>
2022-02-25 00:34:58 +0900

    14장 카테고리를 라우터에 붙이기부터 시작하면됨

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { Fragment, useCallback, useState } from "react";
+import "./App.css";
+import Categories from "./components/Categories";
+import NewsList from "./components/NewsList";
+// const apikey = '8e4a6101174e464794f1214934038e93'
 
 function App() {
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </Fragment>
   );
 }
 
