757cd148630d74d6b08d3ae54473daef82f7d54e
jmeno1011 <jwhltn8282@gmail.com>
2022-02-26 22:42:32 +0900

    usePromis라는 hook을 추가해서 사용해봄

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import usePromise from "../lib/usePromise";
 import NewsItem from "./NewsItem";
 
 const sampleArticle = {
@@ -11,34 +12,45 @@ const sampleArticle = {
 };
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8e4a6101174e464794f1214934038e93`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8e4a6101174e464794f1214934038e93`
+    );
   }, [category]);
+
+  // usePromise 를 사용함으로 아래 useEffect가 필요없어짐
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const query = category === "all" ? "" : `&category=${category}`;
+  //       const response = await axios.get(
+  //         `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8e4a6101174e464794f1214934038e93`
+  //       );
+  //       setArticles(response.data.articles);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기 중..</NewsListBlock>;
   }
   // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  if (!response) {
     return null;
   }
-  // articles 값이 유효 할 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  // response 값이 유효 할 때
+  // {}로 감싼 이유는 object인 response.data안의 articles 의 value를 가져오기 위함
+  const { articles } = response.data;
+  console.log(articles);
   return (
     <NewsListBlock>
       {articles.map((article) => (
